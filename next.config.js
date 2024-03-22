83a949b79e7efa96f64f33d5876ce10903d4543f
jadj <jakub.jadczyk@innsoft.pl>
2024-03-22 11:31:42 +0100

    img

--- next.config.js
+++ next.config.js
@@ -8,15 +8,15 @@ module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   images: {
-    // domains: ['github.blog'],
+    domains: ['github.blog'],
     deviceSizes: [320, 640, 1080, 1200],
     imageSizes: [64, 128],
     unoptimized: true,
     loader: 'imgix',
-    path: 'https://jadczykjakub.github.io/geo-pomiar',
+    path: '/geo-pomiar',
   },
   // output: 'export',
-  // basePath: '/geo-pomiar',
+  basePath: '/geo-pomiar',
   swcMinify: true,
   compiler: {
     styledComponents: true,
