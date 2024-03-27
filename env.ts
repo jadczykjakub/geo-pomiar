d13ffb9318fe7852cffdffa41f23dbc8aab4d7ae
jadczykajakub <jadczyk.jakub@gmail.com>
2024-03-27 20:39:28 +0100

    small changes

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
-  SITE_NAME: 'GEO-POMIAR Walasek',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
+  SITE_NAME: 'Usługi geodezyjno-kartograficzne GEO-POMIAR Walasek',
+  OG_IMAGES_URL: 'https://geo-pomiar-walasek.pl/hero/hero.png',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
   // MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,8 +21,8 @@ export default function Homepage({ posts }: InferGetStaticPropsType<typeof getSt
       <Head>
         <title>{EnvVars.SITE_NAME}</title>
         <meta
-          name="description"
-          content="Tempor nostrud velit fugiat nostrud duis incididunt Lorem deserunt est tempor aute dolor ad elit."
+          name="Firma geodezycjna Geo-Pomiar Walasek"
+          content="Firma geodezyjna, powiat Włoszczowski, usługi geodezyjne, pomiary terenowe, mapa geodezyjna, doradztwo geodezyjne"
         />
       </Head>
       <HomepageWrapper>
