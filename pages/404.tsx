252f6ef49cc0eff43380d815e9dbbca71d6c98f1
jadczykajakub <jadczyk.jakub@gmail.com>
2024-03-27 20:45:50 +0100

    favico

--- pages/404.tsx
+++ pages/404.tsx
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
           <NotFoundIllustration />
         </ImageContainer>
         <Title>404</Title>
-        <Description>Oh, that&apos;s unfortunate! Page not found 😔</Description>
+        <Description>Taka strona nie istnieje 😔</Description>
       </Container>
     </Wrapper>
   );
